35ccda9fb95a93902c621b52475074e55afe1a65
Ramya Pavan <ramya.nagavarapu@gmail.com>
2019-05-24 16:48:10 -0700

    grade

--- Hackerrank/com/practice/java/hackerrank/Grade.java
+++ Hackerrank/com/practice/java/hackerrank/Grade.java
@@ -7,24 +7,16 @@ package com.practice.java.hackerrank;
  * @author Ramy
  *
  */
-
-import java.io.*;
-import java.math.*;
-import java.text.*;
-import java.util.*;
-import java.util.regex.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Grade {
 
 	/*
 	 * Complete the gradingStudents function below.
-	 */
+	 * 3 33 43 67
+	 */ 
 	static int[] gradingStudents(int[] grades) {
-		/*
-		 * Write your code here.
-		 * 
-		 * 
-		 */
 		int len=grades.length;
 		int count, ne5 = 0;
 
@@ -95,7 +87,6 @@ public class Grade {
 	private static final Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) throws IOException {
-		//	BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
 		int n = Integer.parseInt(scan.nextLine().trim());
 
@@ -108,17 +99,6 @@ public class Grade {
 
 		int[] result = gradingStudents(grades);
 
-		/*(for (int resultItr = 0; resultItr < result.length; resultItr++) {
-			bw.write(String.valueOf(result[resultItr]));
-
-			if (resultItr != result.length - 1) {
-				bw.write("\n");
-			}
-		}
-
-		bw.newLine();
-
-		bw.close();*/
 		for (int resultItr = 0; resultItr < result.length; resultItr++) {
 			System.out.println(result[resultItr]);
 		}
--- Hackerrank/com/practice/java/hackerrank/miniMaxSum.java
+++ Hackerrank/com/practice/java/hackerrank/miniMaxSum.java
@@ -3,47 +3,43 @@ import java.util.Scanner;
 
 public class miniMaxSum {
 
-    // Complete the miniMaxSum function below.
-	private static void miniMaxSum(int[] arr) 
-    {
+	// Complete the miniMaxSum function below.
+	private static void miniMaxSum(int[] arr) {
+		long c[] = null, min = 0,max = 0;
+		int len = arr.length;
+		for(int i=0;i<len;i++) {
+			System.out.println(arr[i]);
+			c[i]=arr[i+1]+c[i];
+			System.out.println(c[i]);
+		}
+		for (int j=0;j<c.length;j++)
+		{
+			if(c[j]>c[j+1]) 
+			{ 
+				min=c[j];
+				max=c[j+1];
+			}
+		}
+		System.out.println(min+" "+max);
 
-    	long c[] = null, min = 0,max = 0;
-    	int len = arr.length;
-    		for(int i=0;i<len;i++) {
-    			System.out.println("in for loop 1");
-    			System.out.println(arr[i]);
-    			System.out.println("in for loop 2");
-    			c[i]=arr[i+1]+c[i];
-        		System.out.println(c[i]);
-    		}
-    		for (int j=0;j<c.length;j++)
-    		{
-    			if(c[j]>c[j+1]) 
-    			{ 
-    				min=c[j];
-    				max=c[j+1];
-    			}
-    		}
-    		System.out.println(min+" "+max);
-    		
-    }
+	}
 
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int[] arr = new int[5];
+	public static void main(String[] args) {
+		int[] arr = new int[5];
 
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		String[] arrItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < 5; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
+		for (int i = 0; i < 5; i++) {
+			int arrItem = Integer.parseInt(arrItems[i]);
+			arr[i] = arrItem;
+		}
 
-        miniMaxSum(arr);
+		miniMaxSum(arr);
 
-        scanner.close();
-    }		
-	
+		scanner.close();
+	}		
+
 }
