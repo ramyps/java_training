f390b584023bc90f54a316f7678766fd5f389ee9
Ramya Pavan <ramya.nagavarapu@gmail.com>
2019-06-13 10:20:50 -0700

    web app

--- BankWebApp/src/com/javatraining/apex/BankInfoServlet.java
+++ BankWebApp/src/com/javatraining/apex/BankInfoServlet.java
@@ -18,7 +18,8 @@ public class BankInfoServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+	
 	}
 
 }
